8ca5a1d5116c88b2158c14a509f6b87567336f0c
Joross <jorrossburlas@gmail.com>
2025-09-27 14:04:38 +0800

    added crud to main branch

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::redirect('/','/home');
+
+Route::get('/home',[CustomerController::class,'showAllCustomer'])->name('home');
+ 
+Route::post('/saveCustomer',[CustomerController::class,'saveCustomerDetail'])->name('saveCustomer');
+
+Route::post('/saveCustomerUnsani',[CustomerController::class,'saveCustomerDetailUnsani'])->name('saveCustomerUnsanitized');
+
+Route::get('/editCustomer/{cust_id}',[CustomerController::class,'showCustomerDetails'])->name('customerEdit');
+
+Route::delete('/customerDelete/{cust_id}',[CustomerController::class,'deleteCustomerDetail'])->name('customerDelete');
+
+Route::put('/saveEditCustomer/{cust_id}',[CustomerController::class,'saveEditCustomers'])->name('saveEditCustomer');
+
+
